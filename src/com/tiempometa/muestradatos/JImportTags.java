9f571592b6586b88ba8504145be8a322888047bd
Gerardo Tasistro <gtasistro@tiempometa.com>
2015-08-22 19:01:14 -0500

    rename app title

--- src/com/tiempometa/muestradatos/JImportTags.java
+++ src/com/tiempometa/muestradatos/JImportTags.java
@@ -98,8 +98,7 @@ public class JImportTags extends JDialog {
 	private void initComponents() {
 		// JFormDesigner - Component initialization - DO NOT MODIFY
 		// //GEN-BEGIN:initComponents
-		ResourceBundle bundle = ResourceBundle
-				.getBundle("com.tiempometa.muestradatos.muestradatos");
+		ResourceBundle bundle = ResourceBundle.getBundle("com.tiempometa.muestradatos.muestradatos");
 		dialogPane = new JPanel();
 		contentPanel = new JPanel();
 		label1 = new JLabel();
@@ -117,24 +116,27 @@ public class JImportTags extends JDialog {
 		closeButton = new JButton();
 		CellConstraints cc = new CellConstraints();
 
-		// ======== this ========
+		//======== this ========
 		setTitle(bundle.getString("JImportTags.this.title"));
 		Container contentPane = getContentPane();
 		contentPane.setLayout(new BorderLayout());
 
-		// ======== dialogPane ========
+		//======== dialogPane ========
 		{
 			dialogPane.setBorder(Borders.DIALOG_BORDER);
 			dialogPane.setLayout(new BorderLayout());
 
-			// ======== contentPanel ========
+			//======== contentPanel ========
 			{
-				contentPanel.setLayout(new FormLayout(new ColumnSpec[] {
+				contentPanel.setLayout(new FormLayout(
+					new ColumnSpec[] {
 						FormFactory.DEFAULT_COLSPEC,
 						FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
 						FormFactory.DEFAULT_COLSPEC,
 						FormFactory.LABEL_COMPONENT_GAP_COLSPEC,
-						new ColumnSpec(Sizes.dluX(140)) }, new RowSpec[] {
+						new ColumnSpec(Sizes.dluX(140))
+					},
+					new RowSpec[] {
 						FormFactory.DEFAULT_ROWSPEC,
 						FormFactory.LINE_GAP_ROWSPEC,
 						FormFactory.DEFAULT_ROWSPEC,
@@ -145,18 +147,17 @@ public class JImportTags extends JDialog {
 						FormFactory.LINE_GAP_ROWSPEC,
 						FormFactory.DEFAULT_ROWSPEC,
 						FormFactory.LINE_GAP_ROWSPEC,
-						FormFactory.DEFAULT_ROWSPEC }));
+						FormFactory.DEFAULT_ROWSPEC
+					}));
 
-				// ---- label1 ----
+				//---- label1 ----
 				label1.setText(bundle.getString("JImportTags.label1.text"));
 				label1.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(label1, cc.xy(3, 1));
 
-				// ---- selectImportFileButton ----
-				selectImportFileButton.setText(bundle
-						.getString("JImportTags.selectImportFileButton.text"));
-				selectImportFileButton.setFont(new Font("Tahoma", Font.PLAIN,
-						14));
+				//---- selectImportFileButton ----
+				selectImportFileButton.setText(bundle.getString("JImportTags.selectImportFileButton.text"));
+				selectImportFileButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				selectImportFileButton.addActionListener(new ActionListener() {
 					@Override
 					public void actionPerformed(ActionEvent e) {
@@ -165,42 +166,41 @@ public class JImportTags extends JDialog {
 				});
 				contentPanel.add(selectImportFileButton, cc.xy(5, 1));
 
-				// ---- label2 ----
+				//---- label2 ----
 				label2.setText(bundle.getString("JImportTags.label2.text"));
 				label2.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(label2, cc.xy(3, 3));
 
-				// ---- sheetComboBox ----
+				//---- sheetComboBox ----
 				sheetComboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(sheetComboBox, cc.xy(5, 3));
 
-				// ---- label3 ----
+				//---- label3 ----
 				label3.setText(bundle.getString("JImportTags.label3.text"));
 				label3.setFont(new Font("Tahoma", Font.BOLD, 14));
 				contentPanel.add(label3, cc.xy(3, 5));
 				contentPanel.add(separator1, cc.xy(5, 5));
 
-				// ---- label4 ----
+				//---- label4 ----
 				label4.setText(bundle.getString("JImportTags.label4.text"));
 				label4.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(label4, cc.xy(3, 7));
 
-				// ---- bibComboBox ----
+				//---- bibComboBox ----
 				bibComboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(bibComboBox, cc.xy(5, 7));
 
-				// ---- label5 ----
+				//---- label5 ----
 				label5.setText(bundle.getString("JImportTags.label5.text"));
 				label5.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(label5, cc.xy(3, 9));
 
-				// ---- epcComboBox ----
+				//---- epcComboBox ----
 				epcComboBox.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				contentPanel.add(epcComboBox, cc.xy(5, 9));
 
-				// ---- importButton ----
-				importButton.setText(bundle
-						.getString("JImportTags.importButton.text"));
+				//---- importButton ----
+				importButton.setText(bundle.getString("JImportTags.importButton.text"));
 				importButton.setFont(new Font("Tahoma", Font.PLAIN, 14));
 				importButton.addActionListener(new ActionListener() {
 					@Override
@@ -212,14 +212,17 @@ public class JImportTags extends JDialog {
 			}
 			dialogPane.add(contentPanel, BorderLayout.CENTER);
 
-			// ======== buttonBar ========
+			//======== buttonBar ========
 			{
 				buttonBar.setBorder(Borders.BUTTON_BAR_GAP_BORDER);
-				buttonBar.setLayout(new FormLayout(new ColumnSpec[] {
-						FormFactory.GLUE_COLSPEC, FormFactory.BUTTON_COLSPEC },
-						RowSpec.decodeSpecs("pref")));
+				buttonBar.setLayout(new FormLayout(
+					new ColumnSpec[] {
+						FormFactory.GLUE_COLSPEC,
+						FormFactory.BUTTON_COLSPEC
+					},
+					RowSpec.decodeSpecs("pref")));
 
-				// ---- closeButton ----
+				//---- closeButton ----
 				closeButton.setText("Cerrar");
 				closeButton.addActionListener(new ActionListener() {
 					@Override
